fba601d5de02c4850a554c4445327714be981d6a
Igor Leonenko <igrleon@gmail.com>
2021-03-04 14:02:39 +0100

    add rubocop, setup configuration

--- lib/eps-rapid.rb
+++ lib/eps-rapid.rb
@@ -1,6 +1,20 @@
-require "eps-rapid/version"
+require 'eps-rapid/version'
+require 'eps-rapid/geography'
+require 'digest'
 
 module EpsRapid
-  class Error < StandardError; end
-  # Your code goes here...
+  class << self
+    attr_accessor :api_key, :secret_key, :base_path
+
+    def configure
+      yield(self)
+    end
+
+    def auth_header
+      timestamp    = Time.now.to_i
+      to_be_hashed = "#{self.api_key}#{self.secret_key}#{timestamp}"
+      signature    = Digest::SHA2.new(512).hexdigest(to_be_hashed)
+      auth_header  = "EAN apikey=#{self.api_key},signature=#{signature},timestamp=#{timestamp}"
+    end
+  end
 end
